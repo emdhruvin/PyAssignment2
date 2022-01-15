e13833337910c465db9fc2d55138702a5a6f57c3
Dhruvin Tandel <81218714+emdhruvin@users.noreply.github.com>
2022-01-15 23:22:03 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,28 @@
+# 20CE143 -- DHRUVIN  TANDEL
+#A : create tuple with different data type
+# tup=(1,1.05,'hi!!',[15, 25, 30], {1: 10, 2: 20, 3: 30})
+# for i in range(len(tup)):
+#     print(tup[i])
+# 
+# B : tuple with number and print one item
+# tup=(range(1,10))
+# print(tup[5])
+# 
+# C : add item in tuple
+# tup=(1,3,2,4,5,6)
+# print('before',tup)
+# li=list(tup)#convert tuple to list
+# li.append(9)
+# tup=tuple(li)
+# print('after',tup)
+# 
+#D : convert tuple to string
+# tup=('p','y','t','h','o','n')
+# str=''
+# for item in tup:
+#     str+=item
+# print(str,type(str))
+# 
+#E : find lenth of string
+# tup=(range(1,7,1))
+# print(len(tup))
